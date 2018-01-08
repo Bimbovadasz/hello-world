82ecd403775c25e6f28c418363de6fb3a26af3db
Bimbovadasz <35198213+Bimbovadasz@users.noreply.github.com>
2018-01-08 21:40:46 +0100

    Add files via upload

--- 06_Smasher.py
+++ 06_Smasher.py
@@ -1,15 +1,27 @@
-import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(12,GPIO.IN)
-print "Smash that button!"
-Smash=0 
-try:
+def setup():
+    import RPi.GPIO as GPIO
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(12,GPIO.IN)
+    print "Smash that button!"
+    Smash=0 
+
+def event():
+    global Smash
+    print(Smash)
+    Smash=Smash+1
+    
+def loop():
+    GPIO.add_event_detect(12,GPIO.FALLING,callback=event,bouncetime=300)
     while True:
-        if GPIO.input(12)==0:
-            pass
-        else:
-            Smash=Smash+1
-        print(Smash)
-finally:
+        pass
+
+def kill():
     GPIO.cleanup()
-            
+
+if __name__ == '__main__':
+    setup()
+    try:
+        loop()
+    finally:
+        kill()
+        
