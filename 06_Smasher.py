bf91bc41517209f90e3535804c5cb104b4f85302
Bimbovadasz <35198213+Bimbovadasz@users.noreply.github.com>
2018-01-08 21:04:35 +0100

    Add files via upload

--- 06_Smasher.py
+++ 06_Smasher.py
@@ -2,13 +2,13 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12,GPIO.IN)
 print "Smash that button!"
-Button_smash==0 
+Button_smash == 0 
 try:
     while True:
         if GPIO.input(12)==0:
             pass
         else:
-            Button_smash==Button_smash+1
+            Button_smash == Button_smash+1
             print(Button_smash)
 finally:
     GPIO.cleanup()
