7da06c6a161ee272f988e45604d8c45423019ee4
Bimbovadasz <35198213+Bimbovadasz@users.noreply.github.com>
2018-01-08 14:21:45 +0100

    Add files via upload

--- 04_Lamp.py
+++ 04_Lamp.py
@@ -1,32 +1,18 @@
-def setup():
-    print "Doin it plox"
-    import RPi.GPIO as GPIO
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(11,GPIO.OUT)
-    GPIO.setup(12,GPIO.IN)
-    ledstate=False
+print "Doin it plox"
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11,GPIO.OUT)
+GPIO.setup(12,GPIO.IN)
+ledstate = False
 
-def buttonevent():
-    global ledstate
-    ledstate=not ledstate
-    if ledstate:
-        print "LED on"
-    else:
-        print "LED off"
-    GPIO.output(11,ledstate)
-
-def loop(): 
-    GPIO.add_event_detect(buttonPin,GPIO.FALLING,callback = buttonEvent,bouncetime=300)
+try:
     while True:
-	pass
-    
-def kill():
+        if GPIO.input(12)==0:
+            GPIO.output(11,ledstate)
+        else:
+            ledstate = not ledstate
+            print "Switched"
+finally:
     GPIO.output(11,False)
     GPIO.cleanup()
-
-if __name__ == '__main__':
-    setup()
-    try:
-        loop()
-    finally:
-        kill()
+            
