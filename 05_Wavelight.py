89e534c8c630e53ec627d15116245c5437a9809e
Bimbovadasz <35198213+Bimbovadasz@users.noreply.github.com>
2018-01-08 15:08:03 +0100

    Add files via upload

---
+++ 05_Wavelight.py
@@ -0,0 +1,25 @@
+print "Doin it plox"
+import RPi.GPIO as GPIO
+import time
+ledpins = [3,5,11,13,15,12,16,18,22,24]
+GPIO.setmode(GPIO.BOARD)
+
+for pin in ledpins:
+    GPIO.setup(pin,GPIO.OUT)
+    GPIO.output(pin,False)
+    
+try:
+    while True:
+        for pin in ledpins:
+            GPIO.output(pin,True)
+            time.sleep(0.1)
+            GPIO.output(pin,False)
+        for pin in ledpins[10:0:-1]:
+            GPIO.output(pin,True)
+            time.sleep(0.1)
+            GPIO.output(pin,False)
+finally:
+    for pin in ledpins:
+        GPIO.output(pin,False)
+    GPIO.cleanup()
+        
