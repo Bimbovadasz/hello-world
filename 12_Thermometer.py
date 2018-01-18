085aaf0cee44116aa8043b7550d487bdc356d6a3
Bimbovadasz <35198213+Bimbovadasz@users.noreply.github.com>
2018-01-18 20:34:20 +0100

    Add files via upload

--- 12_Thermometer.py
+++ 12_Thermometer.py
@@ -18,9 +18,9 @@ def loop():
     while True:
         value=analogread(0)
         voltage=(3.3/255)*value
-        R2=10*voltage/(3.3-voltage)
-        T2=1/(1/(273.15+25)+math.log(R2/10)/3950)
-        TC=T2-273.15
+        Rt = 10 * voltage / (3.3 - voltage)
+        tempK = 1/(1/(273.15 + 25) + math.log(Rt/10)/3950.0)
+        TC=tempK-273.15
         print "Hey bro! ADC value: %d, Voltage: %.2f Volts, Temperature: %.2f Celsius" %(value, voltage, TC)
 
 def kill():
