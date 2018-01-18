adfad7d63c458958b82bbd270566205f99686a08
Bimbovadasz <35198213+Bimbovadasz@users.noreply.github.com>
2018-01-18 20:06:47 +0100

    Add files via upload

---
+++ 12_Thermometer.py
@@ -0,0 +1,37 @@
+import RPi.GPIO as GPIO
+import time
+import smbus
+import math
+
+addr=0x48
+cmd=0x40
+bus=smbus.SMBus(1)
+
+def analogread(ch):
+    value=bus.read_byte_data(addr,cmd+ch)
+    return value
+
+def setup():
+    GPIO.setmode(GPIO.BOARD)
+
+def loop():
+    while True:
+        value=analogread(1)
+        voltage=(3.3/255)*value
+        R2=10*voltage/(3.3-voltage)
+        T2=298.15+3950/(math.log(R2/10))
+        TC=T2-273.15
+        print "Hey bro! ADC value: %d, Voltage: %.2f Volts, Temperature: %.2f Celsius" %(value, voltage, TC)
+
+def kill():
+    GPIO.cleanup()
+
+if __name__=="__main__":
+    print "Doin it plox"
+    setup()
+    try:
+        loop()
+    finally:
+        kill()
+
+    
