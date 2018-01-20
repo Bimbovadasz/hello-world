9b2c8876a80f093bf02034b57e4f3f0c0bc32642
Bimbovadasz <35198213+Bimbovadasz@users.noreply.github.com>
2018-01-20 20:12:38 +0100

    Add files via upload

---
+++ 16_RemoteServo2.py
@@ -0,0 +1,43 @@
+import RPi.GPIO as GPIO
+import time
+import smbus
+
+addr=0x48
+cmd=0x40
+bus=smbus.SMBus(1)
+
+def setup():
+    global p
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(12,GPIO.OUT)
+    GPIO.output(12,False)
+    p=GPIO.PWM(12,50)
+    p.start(7.5)
+    
+def analogread():
+    value=bus.read_byte_data(addr,cmd+0)
+    return value
+
+def control():
+    while True:
+        AV=analogread()
+        print(AV)
+        if AV>150:
+            p.ChangeDutyCycle(2.5)
+        elif AV<106:
+            p.ChangeDutyCycle(12.5)
+        else:
+            p.ChangeDutyCycle(7.5)
+
+def kill():
+    p.stop()
+    GPIO.cleanup()
+    bus.close
+
+if __name__=="__main__":
+    print "Doin it plox"
+    setup()
+    try:
+        control()
+    finally:
+        kill()
