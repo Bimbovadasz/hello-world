c6ce703306d2443cad4ef0d2d6e8b06d06cdc01f
Bimbovadasz <35198213+Bimbovadasz@users.noreply.github.com>
2018-05-27 23:16:12 +0200

    Add files via upload

---
+++ PowerButton.py
@@ -0,0 +1,31 @@
+# power button script FTW
+
+def setup():
+    import RPI.GPIO as GPIO
+    import time
+    import subprocess
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(11, GPIO.OUT)
+    GPIO.output(11, True)
+
+def switch():
+    while True:
+        button = GPIO.input(12)
+        if button == True:
+            subprocess.call("shutdown now", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            while True:
+                GPIO.output(11, False)
+                time.sleep(0.5)
+                GPIO.output(11, True)
+                time.sleep(0.5)
+
+def kill():
+    GPIO.cleanup()
+
+if __name__ == '__main__':
+    setup()
+    try:
+        switch()
+    finally:
+        kill()
